60898546ab4549793e0bfb823a7ecd8b21cf36f2
sean-attewell <sean.attewell@gmail.com>
2019-06-05 13:54:24 +0100

    stock prices done

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -5,7 +5,23 @@ import argparse
 
 # takes a list as input
 def find_max_profit(prices):
-    pass
+    max_price = 0
+    min_price = prices[0]
+    for i in range(1, len(prices)):
+        if prices[i] >= max_price:
+            max_price = prices[i]
+            for j in range(0, i):
+                if prices[j] < min_price:
+                    min_price = prices[j]
+
+    profit = max_price - min_price
+
+    # return f"Max price ${max_price}, min price ${min_price}, profit ${profit}"
+    return profit
+
+
+print(find_max_profit([100, 55, 4, 98, 10, 18,
+                       90, 95, 43, 11, 47, 67, 89, 42, 49, 79]))
 
 
 if __name__ == '__main__':
