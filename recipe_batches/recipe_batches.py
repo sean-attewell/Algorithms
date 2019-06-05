9b023bb3bad4bba420327ef5dce6b3b81dcae7f4
sean-attewell <sean.attewell@gmail.com>
2019-06-05 15:20:11 +0100

    recipe batches done

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -2,13 +2,42 @@
 
 import math
 
+
+# def recipe_batches(recipe, ingredients):
+#     batches = 99999999
+#     for ingredient_name, amount_have in ingredients.items():
+#         if amount_have - recipe[ingredient_name] >= 0:
+#             batches_of_this_ingredient = amount_have // recipe[ingredient_name]
+#             if batches_of_this_ingredient < batches:
+#                 batches = batches_of_this_ingredient
+#         else:
+#             batches = 0
+#             return batches
+
+#     return batches
+
 def recipe_batches(recipe, ingredients):
-  pass 
+    batches = 99999999
+    for ingredient_name, amount_required in recipe.items():
+        try:
+            test = ingredients[ingredient_name]
+            if amount_required - ingredients[ingredient_name] <= 0:
+                batches_of_this_ingredient = ingredients[ingredient_name] // amount_required
+                if batches_of_this_ingredient < batches:
+                    batches = batches_of_this_ingredient
+            else:
+                batches = 0
+                return batches
+        except KeyError:
+            batches = 0
+            return batches
+    return batches
 
 
 if __name__ == '__main__':
-  # Change the entries of these dictionaries to test 
-  # your implementation with different inputs
-  recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
-  print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
+    # Change the entries of these dictionaries to test
+    # your implementation with different inputs
+    recipe = {'milk': 100, 'flour': 4, 'sugar': 10, 'butter': 5}
+    ingredients = {'milk': 1288, 'flour': 9, 'sugar': 95}
+    print("{batches} batches can be made from the available ingredients: {ingredients}.".format(
+        batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
